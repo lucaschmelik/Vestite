5ae76254f4e64108eda3868924862cd09beb6dad
PatricioSasso <patonsasso@gmail.com>
2021-05-06 22:42:18 -0300

    Agregue el nuevo proyecto VESTITE

---
+++ ProyectoVestite/Vestite.BLL/UsuarioBLL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vestite.DAL;
+using Vestite.BE;
+using Vestite.Servicios;
+
+namespace Vestite.BLL
+{
+    public class UsuarioBLL
+    {
+        private Usuario _oUsuario;
+        private UsuarioDAL _oUsuarioDAL;
+
+        public UsuarioBLL(string sNombre, string sContraseña)
+        {
+            _oUsuario = new Usuario(sNombre, sContraseña);
+        }
+
+        public void Login()
+        {
+            _oUsuarioDAL = new UsuarioDAL();
+
+            if (SessionManager.Session.IsLogged()) throw new Exception("Existe una sesión iniciada");
+
+            if (_oUsuarioDAL.GetOne().Nombre != _oUsuario.Nombre) throw new Exception("Ingreso un usuario incorrecto");
+
+            if (_oUsuarioDAL.GetOne().Contraseña != _oUsuario.Contraseña) throw new Exception("Ingreso la contraseña incorrecta");
+
+            SessionManager.Session.Login(_oUsuario);
+        }
+    }
+}
---
+++ ProyectoVestite/Vestite.DAL/UsuarioDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vestite.BE;
+
+namespace Vestite.DAL
+{
+    public class UsuarioDAL
+    {
+        public Usuario _oUsuario;
+
+        public UsuarioDAL()
+        {
+            //HARCODEADA
+            _oUsuario = new Usuario("Lucas", "123");
+        }
+
+        public Usuario GetOne()
+        {
+            return _oUsuario;
+        }
+    }
+}
